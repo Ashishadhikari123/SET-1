d022fbbe21bbc46b455966872f8d22938466d6cc
Ashishadhikari123 <adhikarigolu987@gmail.com>
2021-04-04 14:32:50 +0530

    program to find the area of triangle

---
+++ 11.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float l,b;
+    cout<<"Enter length "<<endl;
+    cin>>l;
+    cout<<"Enter breadth "<<endl;
+    cin>>b;
+    cout<<"Area of triangle = "<<(l*b)/2;
+    return 0;
+}
+

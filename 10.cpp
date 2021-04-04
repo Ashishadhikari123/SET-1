52984517b18d9145325c647e54b36d0f280745c1
Ashishadhikari123 <adhikarigolu987@gmail.com>
2021-04-04 14:30:16 +0530

    program to find the area of rectangle

---
+++ 10.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int l,b;
+    cout<<"Enter length of rectangle"<<endl;
+    cin>>l;
+    cout<<"Enter breadth of rectangle"<<endl;
+    cin>>b;
+    cout<<"Area of rectangle = "<<l*b;
+    return 0;
+}

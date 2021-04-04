97cbc6c678a0a18cd635ad6ba3306a6bb49b6c0c
Ashishadhikari123 <adhikarigolu987@gmail.com>
2021-04-04 13:52:40 +0530

    program to print ASCII value of inputted character

---
+++ 3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout<<"Enter character"<<endl;
+    cin>>ch;
+    cout<<"ASCII value of inputted character is "<<(int)ch;
+    return 0;
+}
+

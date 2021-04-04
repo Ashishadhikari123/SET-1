b9387987cbbdf6c49c837a77872e44fd6a5e677f
Ashishadhikari123 <adhikarigolu987@gmail.com>
2021-04-04 14:39:08 +0530

    program to find the sum of n natural numbers

---
+++ 13.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n,sum=0;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+    cout<<"Sum = "<<(n*(n+1))/2;
+    return 0;
+}

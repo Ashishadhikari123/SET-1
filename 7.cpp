a136956ae0488d1eb4fcdca4d386a3ac857b5b3f
Ashishadhikari123 <adhikarigolu987@gmail.com>
2021-04-04 14:12:24 +0530

    program to find whether number is positive or not

---
+++ 7.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter number"<<endl;
+    cin>>n;
+    if(n<0)
+        cout<<"Negative number";
+    else
+        cout<<"Positive number";
+    return 0;
+}
